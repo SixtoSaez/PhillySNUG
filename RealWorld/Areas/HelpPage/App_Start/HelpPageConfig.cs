83ec38840fc1ec06ee97ec5d80800b2b18cd8252
Sixto Saez <sf_saez@msn.com>
2012-09-06 12:46:09 -0400

    Refactored help sample generation to be specific to method usage

--- RealWorld/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ RealWorld/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -1,9 +1,9 @@
 using System;
-using System.Collections.Generic;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Http;
 using RealWorld.Areas.Rest.Models;
-using RealWorld.Models;
 
 namespace RealWorld.Areas.HelpPage
 {
@@ -21,62 +21,106 @@ namespace RealWorld.Areas.HelpPage
                 new XmlDocumentationProvider(
                     HttpContext.Current.Server.MapPath("~/App_Data/RealWorld.xml")));
 
-            //// Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
-            //// Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
-            //// formats by the available formatters.
-            //config.SetSampleObjects(new Dictionary<Type, object>
-            //{
-            //    {typeof(string), "sample string"},
-            //    {typeof(IEnumerable<string>), new string[]{"sample 1", "sample 2"}}
-            //});
+            var controller = "LoginWithLinks";
+            var action = "Get";
+            SetupSampleForControllerAction(
+                config,
+                (sample, mediaType) => config.SetSampleResponse(sample, mediaType, controller, action),
+                GetResponseLinkedCredentials());
 
-            //// Uncomment the following to use "[0]=foo&[1]=bar" directly as the sample for all actions that support form URL encoded format
-            //// and have IEnumerable<string> as the body parameter or return type.
-            //config.SetSampleForType("[0]=foo&[1]=bar", new MediaTypeHeaderValue("application/x-www-form-urlencoded"), typeof(IEnumerable<string>));
+            action = "Post";
+            SetupSampleForControllerAction(
+                config,
+                (sample, mediaType) => config.SetSampleRequest(sample, mediaType, controller, action),
+                PostRequestLinkedCredentials());
 
-            //// Uncomment the following to use "1234" directly as the request sample for media type "text/plain" on the controller named "Values" 
-            //// and action named "Put".
-            //config.SetSampleRequest("1234", new MediaTypeHeaderValue("text/plain"), "Values", "Put");
+            SetupSampleForControllerAction(
+                config,
+                (sample, mediaType) => config.SetSampleResponse(sample, mediaType, controller, action),
+                PostResponseLinkedCredentials());
+        }
 
-            //// Uncomment the following to use the image on "../images/aspNetHome.png" directly as the response sample for media type "image/png"
-            //// on the controller named "Values" and action named "Get" with parameter "id".
-            //config.SetSampleResponse(new ImageSample("../images/aspNetHome.png"), new MediaTypeHeaderValue("image/png"), "Values", "Get", "id");
+        private static void SetupSampleForControllerAction<T>(
+            HttpConfiguration config,
+            Action<object, MediaTypeHeaderValue> configAction,
+            T sample)
+        {
+            var mediaTypeHeaderValue = new MediaTypeHeaderValue("text/json");
+            var jsonMediaTypeFormatter = new JsonMediaTypeFormatter();
 
-            //// Uncomment the following to correct the sample request when the action expects an HttpRequestMessage with ObjectContent<string>.
-            
-            config.SetActualRequestType(typeof(LinkedCredentials), "LoginWithLinksController", "Post");
-            config.SetSampleObjects(
-                new Dictionary<Type, object>
-                    {
-                        {
-                            typeof (LinkedCredentials),
-                            new LinkedCredentials
-                                {
-                                    UserName = "John",
-                                    Password = "some password",
-                                    Links = new[]
-                                                {
-                                                    new AppLink
-                                                        {
-                                                            Rel =
-                                                                "http://appDomain/schema/getOrders",
-                                                            Href = "http://appDomain/api/orders/john",
-                                                            Method = "GET",
-                                                            Description = "Get orders for this user"
-                                                        },
-                                                                                                            new AppLink
-                                                        {
-                                                            Rel =
-                                                                "http://appDomain/schema/getOrderHistory",
-                                                            Href = "http://appDomain/api/orderhistory/john/{cutoffDate}",
-                                                            Method = "GET",
-                                                            Description = "Get order history through this cutoff date"
-                                                        }
+            var jsonResponse = config.GetHelpPageSampleGenerator().WriteSampleObjectUsingFormatter(
+                jsonMediaTypeFormatter,
+                sample,
+                typeof(T),
+                mediaTypeHeaderValue);
 
-                                                }
-                                }
-                            }
-                    });
+            configAction(jsonResponse, mediaTypeHeaderValue);
+
+            mediaTypeHeaderValue = new MediaTypeHeaderValue("application/json");
+            configAction(jsonResponse, mediaTypeHeaderValue);
+
+            mediaTypeHeaderValue = new MediaTypeHeaderValue("text/xml");
+            var xmlMediaTypeFormatter = new XmlMediaTypeFormatter();
+
+            var xmlResponse = config.GetHelpPageSampleGenerator().WriteSampleObjectUsingFormatter(
+                xmlMediaTypeFormatter,
+                sample,
+                typeof(T),
+                mediaTypeHeaderValue);
+
+            configAction(xmlResponse, mediaTypeHeaderValue);
+
+            mediaTypeHeaderValue = new MediaTypeHeaderValue("application/xml");
+            configAction(xmlResponse, mediaTypeHeaderValue);
+        }
+
+        private static LinkedCredentials PostResponseLinkedCredentials()
+        {
+            return new LinkedCredentials
+                       {
+                           UserName = "John",
+                           Password = "validatedToken",
+                           Links = new[]
+                                       {
+                                           new AppLink
+                                               {
+                                                   Rel =
+                                                       "http://appDomain/schema/getOrders",
+                                                   Href = "http://appDomain/api/orders/john",
+                                                   Method = "GET",
+                                                   Description = "Get orders for this user"
+                                               },
+                                           new AppLink
+                                               {
+                                                   Rel =
+                                                       "http://appDomain/schema/getOrderHistory",
+                                                   Href = "http://appDomain/api/orderhistory/john/{cutoffDate}",
+                                                   Method = "GET",
+                                                   Description = "Get order history through this cutoff date"
+                                               }
+
+                                       }
+                       };
+        }
+
+        private static LinkedCredentials GetResponseLinkedCredentials()
+        {
+            return new LinkedCredentials
+            {
+                UserName = "",
+                Password = "",
+                Links = new AppLink[0]
+            };
+        }
+
+        private static LinkedCredentials PostRequestLinkedCredentials()
+        {
+            return new LinkedCredentials
+            {
+                UserName = "John",
+                Password = "aPassword",
+                Links = new AppLink[0]
+            };
         }
     }
 }
